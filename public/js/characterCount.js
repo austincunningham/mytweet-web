ed3a38f40533d7b5a3b8e0abb98631baaf9187da
austincunningham <austincunningham@hotmail.com>
2016-10-14 20:59:28 +0100

    count polling for mytweet page

---
+++ public/js/characterCount.js
@@ -0,0 +1,13 @@
+/**
+ * Created by austin on 12/10/2016.
+ */
+
+//counts down character in textbox
+function CharacterCount(count,output) {
+  //while (output > 0) {
+    //semantic ui form textbox seems to require 29 extra characters to countdown from 140
+    //might be browser related
+    let countdown = 169 - document.getElementById(count).value.length;
+    document.getElementById(output).innerHTML = countdown;
+    console.log(countdown);
+}
