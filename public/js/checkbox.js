3fa4f5227a93a2b1eea308535921ee18bba35c75
austincunningham <austincunningham@hotmail.com>
2016-10-16 16:21:58 +0100

    select all on delete now works

--- public/js/checkbox.js
+++ public/js/checkbox.js
@@ -4,3 +4,12 @@
 $('.ui.checkbox')
     .checkbox()
 ;
+
+// Listen for click on toggle checkbox
+$('#selectAll').click(function(event) {
+  if(this.checked) {
+    $(':checkbox').each(function() {
+      this.checked = true;
+    });
+  }
+});
