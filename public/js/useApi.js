c1d67b986abb20b980491d0b411631c4fe10d5b5
austincunningham <austincunningham@hotmail.com>
2016-10-27 00:41:10 +0100

    adminhome, search for users tweets with api and ajax now working

--- public/js/useApi.js
+++ public/js/useApi.js
@@ -2,34 +2,56 @@
  * Created by austin on 20/10/2016.
  */
 
+/**
+ * Clears the table data
+ */
+function clearTable() {
+  document.getElementById('.tablebody').innerHTML = '';
+}
 
 function populateTable(tweetList) {
   for (var i = 0; i < tweetList.length; i++) {
-    $('#tweet_table tbody ').append('<tr><td><b>' + tweetList[i].name + '</b>  </td><td>'
-        + tweetList[i].message + '</td></tr>');
+    $('.tablebody ').append('<tr><td><b>' + tweetList[i].name + '</b>  </td><td>'
+        + tweetList[i].message + '</td><td class=\"delCheck\" class=\"ui checkbox\"><input name="+_id+" type=\"checkbox\"><label>Delete</label></div></td></tr>');
   }
 }
 
-$('#search_btn').click(function () {
-  var userName = $('#email').val();
-  console.log(userName);
-  $.ajax({
-    dataType: 'json',
-    url: 'http://localhost:4000/api/tweets/email/' + email ,
+ // $('#search-btn').click(function () {
+   // var userName = $('.search-input').val();
+function searchUserTweets(){
+  var email = $('.search-input').val();
+    console.log(email);
+    $.ajax({
+      dataType: 'json',
+      url: 'http://lap-austin:4000/api/tweets/email/' + email,
 
-    success: function (data) {
-      console.log('success');
-      //updateResult(data.length + ' repos');
-      populateTable(data);
-    },
+      success: function (data) {
+        console.log('success');
+        console.log(data);
+        //updateResult(data.length + ' repos');
+        populateTable(data);
+      },
 
-    error: function (err) {
-      //console.log('fail');
-      //console.log(err.statusText);
-      updateResult(userName + ' ' + err.statusText);
-    },
+      error: function (err) {
+        console.log('fail');
+        console.log(err.statusText);
+        //updateResult(userName + ' ' + err.statusText);
+      },
 
-  });
-});
+    });
+  };
 
+function delSelectedTweets(){
+  var ids =[];
+  ids = $('.delCheck').val();
+  console.log(ids);
+  console.log("do i get here")
+  /*for (let i =0; i < ids.length; i++){
+    $.ajax({
+      dataType: 'json',
+      url: 'http://lap-austin:4000/api/tweet/'+ids[i],
+      type : "DELETE",
+    })
+  }*/
+}
 
